e726eff3186c7d90b202179a27ec7233a920a2eb
lightyisu <1513221978@qq.com>
2021-05-01 00:17:29 +0800

    fix display btn

--- components/Nav-btn.js
+++ components/Nav-btn.js
@@ -4,7 +4,7 @@ export default function NavBtn(){
     let [navState,setNavState]=useState(false)
     return (
         <>
-        <div className='rounded-3xl block sm:hidden fixed w-10 cursor-pointer h-10 z-20 bottom-3 right-3 bg-black text-2xl  text-white text-center'
+        <div className='p-2 rounded-3xl block sm:hidden fixed w-10 cursor-pointer h-10 z-20 bottom-3 right-3 bg-black text-2xl  text-white text-center'
          onClick={()=>{setNavState(!navState)}}>{navState?'×':'='}</div>
          <div className='fixed w-full h-full top-0 text-center bg-blue-100  bg-opacity-75 z-10' style={navState?{display:'block'}:{display:'none'}}>
          <ul className='p-10'>
